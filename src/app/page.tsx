35af0acfe5efeecb19eb345c113307e0605b7dfc
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2026-04-16 12:51:38 -0300

    feat: render map with static vehicle markers

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import Map from "../features/vehicles/components/Map";
+
+export default function Home() {
+  return <Map />;
+}
---
+++ src/features/vehicles/components/Map.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import type { LatLngExpression } from "leaflet";
+import type { MapContainerProps } from "react-leaflet";
+
+export default function Map() {
+  const center: LatLngExpression = [-34.6, -58.4];
+
+  const props: MapContainerProps = {
+    center,
+    zoom: 13,
+    style: { height: "100vh", width: "100%" },
+  };
+
+  useEffect(() => {
+    import("leaflet").then((L) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+      L.Icon.Default.mergeOptions({
+        iconRetinaUrl:
+          "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+        iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+        shadowUrl:
+          "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+      });
+    });
+  }, []);
+
+  const vehicles = [
+    { id: 1, position: [-34.6, -58.4], status: "moving" },
+    { id: 2, position: [-34.61, -58.41], status: "idle" },
+    { id: 3, position: [-34.62, -58.42], status: "stopped" },
+  ];
+
+  return (
+    <MapContainer {...props}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+      {vehicles.map((v) => (
+        <Marker key={v.id} position={v.position as [number, number]}>
+          <Popup>
+            Vehicle {v.id} - {v.status}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
