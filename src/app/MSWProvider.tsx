dd7ebf0fbffc150c1ff4008f7b218ccb18165faa
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2026-05-13 18:09:35 -0300

    bugfik/msw

--- src/app/MSWProvider.tsx
+++ src/app/MSWProvider.tsx
@@ -9,15 +9,11 @@ export function MSWProvider({ children }: { children: React.ReactNode }) {
   const [ready, setReady] = useState(false);
 
   useEffect(() => {
-    if (process.env.NODE_ENV === "development") {
-      import("@/mocks/browser").then(({ worker }) => {
-        worker.start({ onUnhandledRequest: "bypass" }).then(() => {
-          setReady(true);
-        });
+    import("@/mocks/browser").then(({ worker }) => {
+      worker.start({ onUnhandledRequest: "bypass" }).then(() => {
+        setReady(true);
       });
-    } else {
-      setReady(true);
-    }
+    });
   }, []);
 
   if (!ready) return null;
